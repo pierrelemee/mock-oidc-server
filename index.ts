bd5a3c137c98927f81c8166051ea803739665030
Pierre LEMEE <pierre@pierrelemee.fr>
2025-01-27 20:33:55 +0100

    🎨 Templating with Twig & Bootstrap

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import * as process from "node:process";
+import consolidate from "consolidate";
+import __dirname from "path";
+import {home} from './routes/home'
 
 const app = express();
 
-app.get("/", (request: Request, response: Response) => {
-  response.status(200).send("Hello World");
-});
+app.engine('twig', consolidate.twig);
+app.set('views', __dirname + '/../views/');
+
+app.get('/', home)
 
 const PORT = parseInt(process.env.PORT, 10) || 9998
 const HOST = process.env.HOST || "localhost";
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}`);
+    console.log(`Server running at http://${HOST}:${PORT}`);
 }).on("error", (error) => {
-  // gracefully handle error
-  throw new Error(error.message);
+    // gracefully handle error
+    throw new Error(error.message);
 });
---
+++ routes/home.ts
@@ -0,0 +1,6 @@
+import {Request, Response} from "express";
+
+
+export function home(req: Request, res: Response) {
+    res.render('home.html.twig');
+}
